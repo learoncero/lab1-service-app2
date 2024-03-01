7d86657affd25cfa6fb2c92b373bddca75c7f772
Lea Roncero <lea.roncero@students.fhv.at>
2024-03-01 14:34:50 +0100

    added rest-service

--- command-line-runner/src/main/java/lab1/commandlinerunner/mycommandline/AnotherCommandLineRunner.java
+++ command-line-runner/src/main/java/lab1/commandlinerunner/mycommandline/AnotherCommandLineRunner.java
@@ -1,4 +1,4 @@
-package lab1.commandlinerunner;
+package lab1.commandlinerunner.mycommandline;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -6,15 +6,15 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Order(1)
+@Order(2)
 @Component
-public class MyCommandLineRunner implements CommandLineRunner {
+public class AnotherCommandLineRunner implements CommandLineRunner {
 
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public void run(String... args) {
-        logger.info("MyCommandLineRunner executed as a Spring Component");
+        logger.info("AnotherCommandLineRunner executed as a Spring Component");
     }
 
 }
